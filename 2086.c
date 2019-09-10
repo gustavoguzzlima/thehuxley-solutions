10c4395c19c9db284fb55d4e58dc677e928714a9
gustavoguzzlima <gustavodl@outlook.com.br>
2019-09-10 09:24:44 -0300

    added Q2086 solution

---
+++ 2086.c
@@ -0,0 +1,40 @@
+/* 
+    (c) 2019 @gustavoguzzlima <gdjl@ic.ufal.br>. The Huxley Q2086.
+*/
+#include <stdio.h>
+
+#define ARRAY_LEN 1000
+
+int main()
+{
+    int array[ARRAY_LEN];
+    int again = 0;
+    int i;
+
+    do 
+    {
+        // read array numbers
+        for (i = 0; i < ARRAY_LEN; i++)
+        {
+            scanf("%d", &array[i]);
+            if (array[i] == -1) return 0;
+        }
+
+        // look for
+        int search;
+        scanf("%d", &search);
+
+        int count = 0;
+        for (i = 0; i < ARRAY_LEN; i++)
+        {
+            if (array[i] == search) count++;
+        }
+
+        // print result
+        printf("%d appeared %d times\n", search, count);
+        count = 0;
+
+    } while (again != -1);
+
+    return 0;
+}
