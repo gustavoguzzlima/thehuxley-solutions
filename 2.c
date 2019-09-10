761682fba59b72a7e35e36e371a5e358a14862f2
gustavoguzzlima <gustavodl@outlook.com.br>
2019-09-10 09:52:21 -0300

    added solution to problem 2

---
+++ 2.c
@@ -0,0 +1,43 @@
+/**
+ * (c) 2019 @gustavoguzzlima <gdjl@ic.ufal.br>. The Huxley Q2.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c, tmp, i, j;
+
+    //input
+    scanf("%d%d%d", &a, &b, &c);
+
+    // assuming it's already sorted...
+
+    if (a > b || a > c || b > c)
+    {
+        if (a > b)
+        {
+            tmp = a;
+            a = b;
+            b = tmp;
+        }
+        if (a > c)
+        {
+            tmp = c;
+            c = a;
+            a = tmp;
+        }
+
+        if (b > c)
+        {
+            tmp = b;
+            b = c;
+            c = tmp;
+        }
+    }
+
+    // output
+    printf("%d\n%d\n%d\n", a, b, c);
+
+    return 0;
+}
