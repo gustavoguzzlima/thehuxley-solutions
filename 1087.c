f1c147134873a2fb7d132100b7ecfb76810ce5c5
gustavoguzzlima <gustavodl@outlook.com.br>
2019-09-10 09:28:03 -0300

    added Q1087 solution

---
+++ 1087.c
@@ -0,0 +1,16 @@
+/*
+    (c) 2019 @gustavoguzzlima <gdjl@ic.ufal.br>. The Huxley Q1087.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    for (i = 0; i < 100; i++) 
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
