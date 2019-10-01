3b15e0798622b649afc328df40616e3f4c639e5e
gustavoguzzlima <gustavodl@outlook.com.br>
2019-10-01 16:16:26 -0300

    solved

---
+++ 396.c
@@ -0,0 +1,31 @@
+/* 
+    (c) 2019 @gustavoguzzlima <gdjl@ic.ufal.br>. Problem 2086, The Huxley.
+*/
+
+#include <stdio.h>
+
+int main() 
+{
+    int taxed_homes = 0;
+    float tax_sum = 0;
+
+    // input
+    int input = 0;
+
+    while (1)
+    {
+        scanf("%d", &input);
+        if (input == 999) break;
+
+        if (input > 2)
+        {
+            tax_sum += ((12.89) * (float)(input-2));
+            taxed_homes += 1;
+        }
+    }
+
+    // output
+    printf("%.2f\n%d\n", tax_sum, taxed_homes);
+  
+    return 0;
+}
