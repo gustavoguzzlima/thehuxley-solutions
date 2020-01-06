183ea9922c6f84df96f5a5a919ba7f48dcdf80ba
Gustavo Lima <gustavodl@outlook.com.br>
2020-01-06 15:02:41 -0300

    added problem 17 solution

---
+++ 17.c
@@ -0,0 +1,15 @@
+/**
+ * 2020 @gustavoguzzlima <gdjl@ic.ufal.br>
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int number;
+    
+    scanf("%d", &number);
+    printf("%d %d\n", number-1, number+1);
+    
+    return 0;
+}
